cda14a5d03e032f881bfa2828d0995eb734f6fea
Chris Rabl <rabl.christopher@gmail.com>
2018-10-01 23:49:25 -0600

    feat: basic server and route declarations

---
+++ routes.js
@@ -0,0 +1,33 @@
+function register(req, res) {
+  res.json({});
+}
+
+function login(req, res) {
+  res.json({});
+}
+
+function logout(req, res) {
+  res.json({});
+}
+
+function events(req, res) {
+  res.json({});
+}
+
+function event(req, res) {
+  res.json({});
+}
+
+function routes(router) {
+  router.post('/register', register);
+  router.post('/login', login);
+  router.post('/logout', logout);
+  router.get('/events', events);
+  router.get('/events/:event_id', event);
+
+  return router;
+}
+
+module.exports = {
+  routes
+};
